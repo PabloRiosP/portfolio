1c50dd13d613abe45d668eb78f14738d4ac77260
PabloRiosP <pablo_rios_p@protonmail.com>
2022-03-07 07:26:39 -0300

    Mejoras en frontend

--- static/js/home.js
+++ static/js/home.js
@@ -1,20 +1,68 @@
-// global lock, so put this code in a closure of some sort so you're not polluting.
-let locked = false;
-let lastCall = false;
+var locked = false;
 
-function runOnScroll(element) {
-    if(locked) return;
+function transitions() {
+    // Webpage Elements
+    let header = document.getElementById("welcome_screen");
+    let btns = document.getElementById("welcome_buttons");
+    let bar = document.getElementById("nav-bar");
 
-    if (lastCall) clearTimeout(lastCall);
-    lastCall = setTimeout(() => {
-        runOnScroll(element);
-        // you do this because you want to handle the last
-        // scroll event, even if it occurred while another
-        // event was being processed.
-    }, 200);
+    // Element Attributes
+    let hdheight = parseInt(window.getComputedStyle(header, null).getPropertyValue("height").toString());
+    let btnsheight = window.getComputedStyle(btns, null).getPropertyValue("height").toString();
 
-    // ...your code goes here...
+    // Attributes calculation for transitions
+    hdheight = parseInt((hdheight*2)/3);
 
+    // Applying immediate trasnsitions
+    btns.style.height = btnsheight;
+    btns.style.opacity = 0.0;
 
-    locked = false;
-};
+    // Applying transitions with delay
+    // 0.5s
+    setTimeout(function () {
+        header.style.height = `${hdheight}px`;
+        header.style.top = "50px";
+        bar.style.top = "0px";
+        btns.style.transition = "height 0.5s";
+        btns.style.height = "0px";
+    }, 500);
+
+    // 1s
+    setTimeout(function () {
+        btns.remove();
+    }, 1000);
+}
+
+// Transition will be done if was not yet done before.
+// If is activated with the mouse wheel conditions are
+// not needed.
+function onWheelReaction() {
+    locked = true;
+    transitions();
+}
+
+// With keyboard transition is enabled just with Down and Up
+// arrows, other keys are ignored.
+function onKeyDownReaction(e) {
+    let k = e.key.toString();
+    if (k === 'ArrowDown' || k === 'ArrowUp') {
+        locked = true;
+        transitions();
+    }
+}
+
+// Event listeners.
+// Any of those are activates just if no one are activated
+// before. Obviously as a frontend js code is reset with a
+// page reload, consistent with the css reload.
+document.addEventListener('wheel', function(e) {
+    if (!locked) {
+        onWheelReaction();
+    }
+});
+
+document.addEventListener('keydown', function(e) {
+    if (!locked) {
+        onKeyDownReaction(e);
+    }
+});
